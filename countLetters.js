0a70276ab8d800aa7c09cffe55c0a3527e91ef66
Zola <zolamcadie@gmail.com>
2017-06-28 18:37:52 +0000

    added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+var input = process.argv.slice(2).join("").toLowerCase().split("");
+console.log(input)
+
+function countLetters(string) {
+  var letterCount = {};
+
+  string.forEach(function(letter) {
+    if (letterCount[letter] === undefined) letterCount[letter] = 0
+    letterCount[letter] += 1
+  });
+
+  return letterCount;
+};
+
+console.log(countLetters(input));
